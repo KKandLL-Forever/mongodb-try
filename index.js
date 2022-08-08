fa2150a6498951d826482f31bc9af8d3a6988bfe
KKandLL-Forever <clay1wang@gmail.com>
2022-08-09 00:05:57 +0800

    init mongodb

---
+++ index.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+// Connection URI
+const uri = "mongodb://127.0.0.1:27017";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    let res = await client
+      .db("test")
+      .collection('inventory')
+      .findOne({item:"journal"})
+    console.log(res)
+  } catch (err) {
+    console.log('Connect failed')
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run()
